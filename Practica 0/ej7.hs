7417c74b812a75f2708c6b4de8347eea32a7befb
Kalashnikovni <denise.marzorati@gmail.com>
2017-08-29 22:18:42 -0300

    Añade ejercicio 7

---
+++ Practica 0/ej7.hs
@@ -0,0 +1,44 @@
+import Data.Text
+import Text.PrettyPrint.HughesPJ as PJ
+import TigerSres
+import TigerTips
+
+-- >> ¿Los tipos son para tiger, digamos en minúsculas?
+-- >> ¿Qué es RW y RO?
+-- >> ¿Unique lo ponemos como otro tipo distinto a Int?
+-- >> ¿Formato de los arrays, alguna sugerencia?
+-- >> ¿Para qué es el Unique del Record?
+-- >> ¿Qué es un RefRecord?
+-- >> ¿Qué es un TTipo?
+printEnv :: EnvEntry -> Doc
+printEnv (Var ve)                = printTipo ve
+-- El tipo de ++ sería por ejemplo: string -> string -> string
+printEnv (Func (_, _, lt, t, _)) = parens (hcat (Prelude.map (\x -> printTipo x <> text " -> ") lt)) <>
+                                   printTipo t
+
+printTipo :: Tipo -> Doc
+printTipo TUnit         = text "unit"
+printTipo TNil          = text "nil"
+printTipo (TInt r)      =
+    case r of
+        RW -> text "int RW"
+        RO -> text "int RO"
+printTipo TString       = text "string"
+printTipo (TArray t _)  = printTipo t <> text "int"
+printTipo (TRecord l _) = braces (fieldsRecord l)
+--printTipo (RefRecord t) =
+--printTipo (TTipo t)     =
+
+fieldsRecord :: [(Text, Tipo, Int)] -> Doc
+fieldsRecord []     = PJ.empty
+fieldsRecord [x]    = fR x   
+fieldsRecord (x:xs) = fR x <>
+                      text ", "
+
+fR (te, ti, i) = text (unpack te) <>
+                 text ":" <>
+                 printTipo ti <>
+                 text "int" 
+
+
+
